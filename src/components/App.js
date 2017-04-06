0c42000e70ec1492b31ebecb75bb3d443e2c68de
JonCass <JCass1980@gmail.com>
2017-04-05 19:10:24 -0700

    just need to fix flex

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,37 @@ import store from '../store'
 import {getData} from '../api/data'
 
 const styles = {
-
+  itemFence:{
+    width: 1440,
+    display: 'flex',
+    flexWrap:'wrap'
+  },
+  itemBox : {
+    width: 264,
+    height: 262,
+    margin: '10px 24px 4px 0',
+    border: 'solid #CFCBC8 1px',
+    overflow: 'hidden',
+    backgroundColor: 'white',
+    cursor: 'pointer',
+  },
+  image: {
+    margin: 0,
+    width: 264,
+    height: 212,
+    backgroundPosition: 'center center',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: '100%',
+  },
+  imageLabel: {
+    fontSize: 12,
+    margin: 0,
+    height: 50,
+    width: '100%',
+    backgroundColor: '#FFFFFF',
+    border: 'solid #CFCBC8 0px',
+    borderBottom: 1
+  }
 }
 
 export default React.createClass({
@@ -27,23 +57,18 @@ export default React.createClass({
   },
   render() {
     return (
-      <div>
-        <div>
+        <div styles={styles.itemFence}>
           {this.state.data.map(data=>(
-            <div style={styles.itemBox}>
-              <div img src={data.Images.url_570xN} style={styles.imageBox}/>
-              <div style={styles.infoBox}>
-                <span style={styles.itemTitle}>{data.title}</span>
-                <div styles={styles.storePriceBox}>
-                  <span styles={styles.store}>{data.state}</span>
-                  <span styles={styles.price}>{data.price}</span>
+            <div style={styles.itemBox} key={data.listing_id}>
+              <img src={data.Images[0].url_570xN} style={styles.image} alt={data.listing_id}/>
+              <div className='imageLabel'>
+                <span className='title'>{data.title}</span>
+                <span className='maker'>{data.Shop.shop_name}</span>
+                <span className='price'>{data.price}</span>
                 </div>  
               </div>
-            </div>
-            //<li key={data.listing_id}>{data.price}</li>
           ))}
         </div>
-      </div>
     )
   }
 })
